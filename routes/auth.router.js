cad0a9521e6cd1575d45c547222d208458011176
Ngo Van Tai <internit00@okxe.vn>
2021-05-22 17:21:10 +0700

    authentication api

---
+++ routes/auth.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth.controller')
+const registerUserSchema = require('../schema/registerUser.json')
+const loginUserSchema = require('../schema/loginUser.json')
+const refreshUserSchema = require('../schema/refreshTokenUser.json')
+const validator = require('../middlewares/validate.mdw')
+
+router.post('/register', validator(registerUserSchema), authController.register)
+
+router.post('/login', validator(loginUserSchema), authController.login)
+
+router.post('/refresh', validator(refreshUserSchema), authController.refresh)
+
+module.exports = router; 
