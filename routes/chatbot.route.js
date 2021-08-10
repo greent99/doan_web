9cb4b90b55badf0d733b30923bf8a3d28da46a12
tvcong0999 <tvcong0999@gmail.com>
2021-08-10 22:47:52 +0700

    Revert "test chatbot"
    This reverts commit 98188795f457710aaa9e6c6e373f06a838b93a7c.

--- routes/chatbot.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatBotController = require('../controllers/chatbot.controller');
-
-router.post('/chatbot', chatBotController.postMessage);
-
-router.get('/webhock', chatBotController.getMessage);
-
-module.exports = router; 
